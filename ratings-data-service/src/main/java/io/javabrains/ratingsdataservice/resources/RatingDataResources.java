94d1376c8a97cd86d8239546969c2164adcc6f3b
kubilaycicek <kubilaycicek0693@gmail.com>
2021-01-14 21:21:47 +0300

    Ratings Service getUserRatings

--- ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingDataResources.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingDataResources.java
@@ -5,11 +5,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingDataResources {
+
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, "4");
+    }
+
+    @RequestMapping("/users/{userId}")
+    public List<Rating> getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", "5"),
+                new Rating("5678", "6")
+        );
+        return ratings;
     }
 }
