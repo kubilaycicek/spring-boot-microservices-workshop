3f7f30d201af46a751ca725da692c9218f7d32eb
kubilaycicek <kubilaycicek0693@gmail.com>
2021-01-18 20:58:49 +0300

    create discovery-server

--- discovery-server/src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/io/javabrains/discoveryserver/DiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package io.javabrains.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class DiscoveryServerApplication {
 	}
 
 }
+
